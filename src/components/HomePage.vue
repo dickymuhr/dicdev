e5682d09aa14044b58d2a93ea743bd7132784383
Dicky Muhammad Rizal <dickymuhrizal@gmail.com>
2023-12-16 16:16:08 +0700

    add social link

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -4,16 +4,20 @@
       <img :src="profileImage" alt="Profile">
     </div>
     <div class="description-column">
-      <!-- This is where your introduction and links will go -->
-      <h2>Hello, I'm Dicky, An Aspiring Data Engineer</h2>
+      <!-- Introduction -->
+      <h2>Hello, I'm Dicky! An Aspiring Data Engineer</h2>
       <p>Wanna see my portfolio?</p>
-      <!-- Social links -->
+      <!-- Social links with icons -->
       <div class="links">
-        <!-- Replace '#' with actual links -->
-        <a href="#" class="social-link linkedin">LinkedIn</a>
-        <a href="#" class="social-link github">GitHub</a>
-        <a href="#" class="social-link medium">Medium</a>
-        <!-- More links as needed -->
+        <a href="https://www.linkedin.com/in/dickymuhr/" target="_blank" rel="noopener noreferrer" class="social-link">
+          <img :src="linkedinIcon" alt="LinkedIn">
+        </a>
+        <a href="https://github.com/dickymuhr" target="_blank" rel="noopener noreferrer" class="social-link">
+          <img :src="githubIcon" alt="GitHub">
+        </a>
+        <a href="https://medium.com/@dickymuhrizal" target="_blank" rel="noopener noreferrer" class="social-link">
+          <img :src="mediumIcon" alt="Medium">
+        </a>
       </div>
     </div>
   </div>
@@ -24,8 +28,10 @@ export default {
   name: 'HomePage',
   data() {
     return {
-      // This will include the image via webpack
-      profileImage: require('@/assets/img/profile.png')
+      profileImage: require('@/assets/img/profile.png'),
+      linkedinIcon: require('@/assets/img/LinkedIn.png'),
+      githubIcon: require('@/assets/img/GitHub.png'),
+      mediumIcon: require('@/assets/img/Medium.png'),
     };
   }
 };
@@ -36,8 +42,7 @@ export default {
   display: flex;
   align-items: center; /* This will vertically center the children */
   justify-content: center; /* This will horizontally center the children */
-  min-height: 100vh; /* This ensures that the container is at least the height of the viewport */
-  background-color: #04192C;
+  height: 100%; /* This ensures that the container is at least the height of the viewport */
   color: #ffffff;
   padding: 0;
   margin: 0;
@@ -57,7 +62,7 @@ export default {
   flex-grow: 1; /* Allows the description column to take up the remaining space */
   text-align: left; /* Aligns the text to the left */
   padding-left: 20px; /* Adds spacing between the columns */
-  max-width: 66%; /* Sets the max width to enforce the 1:2 ratio */
+  max-width: 54%; /* Sets the max width to enforce the 1:2 ratio */
 }
 
 h2 {
@@ -67,15 +72,37 @@ h2 {
 
 p {
   margin: 0 0 20px; /* Adjust the margin as needed for your design */
+  font-size: 1.4rem;
 }
 
-.links a {
-  display: inline-block;
-  margin-right: 10px;
-  text-decoration: none; /* Removes the underline from links */
-  color: #ffffff; /* Sets the text color */
-  /* Additional styles for your links */
+.links {
+  display: flex; /* Use flexbox for the links container */
+  justify-content: flex-start; /* Align links to the start */
+  align-items: center; /* Center links vertically */
+  gap: 10px; /* Creates a gap between each link */
 }
+
+.links a.social-link {
+  display: flex; /* Use flexbox for individual link alignment */
+  justify-content: center; /* Center the content horizontally */
+  align-items: center; /* Center the content vertically */
+  width: 54px; /* Fixed width for the link background */
+  height: 54px; /* Fixed height for the link background */
+  background-color: #ffffff; /* White background */
+  border-radius: 4px; /* Rounded corners */
+  overflow: hidden; /* Ensures no overflow from the content */
+}
+
+.links a.social-link img {
+  width: 80%; /* Scaled down size, adjust as needed */
+  height: auto; /* Maintain aspect ratio */
+  object-fit: contain; /* Ensures the image fits within the element's box */
+}
+
+.links a.social-link:hover {
+  background-color: #e1e1e1; /* Light grey background on hover */
+}
+
 
 /* Responsive styles and additional styling as needed */
 </style>
